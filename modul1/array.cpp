0ed5b93aa3f06a60cc00b5033f3af674132eea07
Galih <kayyis313@gmail.com>
2025-10-02 08:11:49 +0700

    FIx_modul1

---
+++ modul1/array.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // --- Array 1 Dimensi ---
+    int arr[5] = {10, 20, 30, 40, 50};
+    cout << "Array 1 Dimensi:" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << "Element ke-" << i << ": " << arr[i] << endl;
+    }
+    cout << endl;
+
+    // --- Array 2 Dimensi ---
+    int arr2D[2][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+    };
+    cout << "Array 2 Dimensi:" << endl;
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << "arr2D[" << i << "][" << j << "]: " << arr2D[i][j]
+            << " ";
+        }
+        cout << endl;
+    }
+    // --- Array Multi Dimensi (3D) ---
+    int arr3D[2][2][3] = {
+        { {1, 2, 3}, {4, 5, 6} },
+        { {7, 8, 9}, {10, 11, 12} },
+    };
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            for (int k = 0; k < 3; k++) {
+                cout << "arr3D[" << i << "][" << j << "]["
+                << k << "]: " << arr3D[i][j][k] << endl;
+            }
+        }
+    }
+
+    return 0;
+}
--- modul1/soal1.cpp
+++ modul1/soal1.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
@@ -15,20 +14,3 @@ int main() {
         cout << "Pembagian: tidak dapat dilakukan (pembagi nol)" << endl;
     return 0;
 }
-=======
-#include <iostream>
-using namespace std;
-int main() {
-    float a, b;
-    cout << "Masukkan dua bilangan float: ";
-    cin >> a >> b;
-    cout << "Penjumlahan: " << a + b << endl;
-    cout << "Pengurangan: " << a - b << endl;
-    cout << "Perkalian: " << a * b << endl;
-    if (b != 0)
-        cout << "Pembagian: " << a / b << endl;
-    else
-        cout << "Pembagian: tidak dapat dilakukan (pembagi nol)" << endl;
-    return 0;
-}
->>>>>>> 6c304cbfa1d3038d322e378e56af09aee21ab5a0
