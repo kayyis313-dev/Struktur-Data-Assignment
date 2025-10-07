4302dbf06ffe923a3d133d190ea682fc5756490a
Galih <kayyis313@gmail.com>
2025-10-07 20:07:55 +0700

    Fix_modul1

--- modul1/soal2.cpp
+++ modul1/soal2.cpp
@@ -1,25 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    string satuan[9] = {"satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan",};
-    string belasan[9] = {"sebelas", "dua belas", "tiga belas", "empat belas", "lima belas", "enam belas", "tujuh belas", "delapan belas", "sembilan belas"};
-    string puluhan[9] = {"sepuluh", "dua puluh", "tiga puluh", "empat puluh", "lima puluh", "enam puluh", "tujuh puluh", "delapan puluh", "sembilan puluh"};
+int main() {
+    string satuan[9] = {"satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan"};
+    string belasan[9] = {"sebelas", "dua belas", "tiga belas", "empat belas", "lima belas", 
+                         "enam belas", "tujuh belas", "delapan belas", "sembilan belas"};
+    string puluhan[9] = {"sepuluh", "dua puluh", "tiga puluh", "empat puluh", "lima puluh", 
+                         "enam puluh", "tujuh puluh", "delapan puluh", "sembilan puluh"};
 
     int angka;
-    cout << "Input Angka: ";
+    cout << "Masukkan angka (0 - 100): ";
     cin >> angka;
 
-    if (angka < 10) {
+    if (angka < 0 || angka > 100) {
+        cout << "Angka di luar jangkauan!" << endl;
+    } else if (angka == 0) {
+        cout << "nol" << endl;
+    } else if (angka < 10) {
         cout << satuan[angka - 1] << endl;
+    } else if (angka == 10) {
+        cout << "sepuluh" << endl;
     } else if (angka < 20) {
         cout << belasan[angka - 11] << endl;
-    } else if (angka %10 == 0) {
-        cout << puluhan[angka / 10 - 1] << endl;
+    } else if (angka % 10 == 0 && angka < 100) {
+        cout << puluhan[(angka / 10) - 1] << endl;
+    } else if (angka > 20 && angka < 100) {
+        cout << puluhan[(angka / 10) - 1] << " " << satuan[(angka % 10) - 1] << endl;
     } else if (angka == 100) {
         cout << "seratus" << endl;
-    } else if (angka > 20 && angka < 100) {
-        cout << puluhan[ (angka/10)-1 ] << " " << satuan[ (angka%10)-1 ] << endl;
     }
 
     return 0;
