44edbe395eedbb8ec6fcb1c451e9e1ece0cd0ae8
Galih <kayyis313@gmail.com>
2025-10-09 01:18:03 +0700

    Fix_modul2

---
+++ modul2.cpp/soal2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void tukarNilai(int *p, int *q, int *r) {
+    int temp = *r;
+    *r = *q;
+    *q = *p;
+    *p = temp;
+}
+
+int main() {
+    int a = 7, b = 14, c = 21;
+
+    cout << "Nilai awal:\n";
+    cout << "a = " << a << ", b = " << b << ", c = " << c << endl;
+
+    tukarNilai(&a, &b, &c);
+
+    cout << "\nNilai setelah ditukar (versi pointer):\n";
+    cout << "a = " << a << ", b = " << b << ", c = " << c << endl;
+
+    return 0;
+}
