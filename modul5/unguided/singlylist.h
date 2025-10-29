5930251fe92d2853646618bbbfa4121bba8ba4de
kayyis313-dev <kayyisthoriq@gmail.com>
2025-10-30 02:48:47 +0700

    Refactor singly list header file structure

--- modul5/unguided/singlylist.h
+++ modul5/unguided/singlylist.h
@@ -4,25 +4,21 @@
 #include <iostream>
 using namespace std;
 
-// Struktur untuk elemen (node)
 struct Node {
     int info;
     Node *next;
 };
 
-// Struktur untuk list
 struct List {
     Node *first;
 };
 
-// Deklarasi fungsi dasar
 void createList(List &L);
 Node* alokasi(int x);
 void dealokasi(Node *P);
 void insertFirst(List &L, Node *P);
 void printInfo(List L);
 
-// Tambahan fungsi dari latihan 5.2
 Node* findElm(List L, int x);
 int totalInfo(List L);
 
